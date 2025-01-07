206b65ac7e6ee80ee387ed661c09bf0e9a8ec37d
Arif .r.hakim <hakimqwerti19@gmail.com>
2025-01-07 11:56:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function searchGallery(containerId, inputId) {
+  const input = document.getElementById(inputId).value.toLowerCase();
+  const container = document.getElementById(containerId);
+  const items = container.getElementsByClassName("gallery-item");
+
+  for (let i = 0; i < items.length; i++) {
+    const itemName = items[i].getAttribute("data-name").toLowerCase();
+    if (itemName.includes(input)) {
+      items[i].style.display = "";
+    } else {
+      items[i].style.display = "none";
+    }
+  }
+}
